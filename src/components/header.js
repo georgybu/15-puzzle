4cc7d3c572e7796c466c4871e76519816da3d898
Georgy Bunin <bunin.co.il@gmail.com>
2017-05-07 00:49:42 +0300

    set base dynamically

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,17 @@ const header = {
   template: `
 		<div class="row">
       <div class="small-12 columns">
-        <a href="/#/game" class="button radius" ng-class="{ 'secondary': !$ctrl.isCurrent('/game') }">
+        <a href="#/game" class="button radius" ng-class="{ 'secondary': !$ctrl.isCurrent('/game') }">
             <i class="fa fa-gamepad" aria-hidden="true"></i>
             <!---->
             Game
         </a>
-        <a href="/#/leaders" class="button radius" ng-class="{ 'secondary': !$ctrl.isCurrent('/leaders') }">
+        <a href="#/leaders" class="button radius" ng-class="{ 'secondary': !$ctrl.isCurrent('/leaders') }">
             <!--<i class="fa fa-star" aria-hidden="true"></i>-->
             <i class="fa fa-trophy" aria-hidden="true"></i>
             Leaders
         </a>
-        <a href="/#/settings" class="button radius info right">
+        <a href="#/settings" class="button radius info right">
           <i class="fa fa-cog" aria-hidden="true"></i>
           Settings
         </a>
